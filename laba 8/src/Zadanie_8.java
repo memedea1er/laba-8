0298c118915e6fbdb83ed6c16e37cc6a388c05a2
Глеб Синьковский <gleb030804@gmail.com>
2022-12-13 20:25:49 +0500

    1

--- laba 8/src/Zadanie_8.java
+++ laba 8/src/Zadanie_8.java
@@ -1,49 +1,60 @@
 import java.util.*;
 
 public class Zadanie_8 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите порядок матрицы");
-        int n = in.nextInt();
-        Integer[][] matrix = new Integer[n][n];
-        int p=1, i=0, j=n-1;
-        for (int k=0; k<n; k++) {
-            while (i<n & j<n) {
-                matrix[i][j]=p;
-                i++; j++; p++;
+    public static void vivod (Double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(String.format("%.3f", matrix[i][j]).replace(',', '.') + " ");
             }
-            if (i==n & j==n) {
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+    public static void block(Double[][] matrix, double na, double ko, int ni, int nj) {
+        int n = matrix.length/2, i=0+ni, j=n-1+nj;
+        double p=na, sh=(ko-na)/(n*n-1);
+        for (int k=0; k<n; k++) {
+            while (i<n+ni & j<n+nj) {
+                matrix[i][j]=p;
+                i++; j++; p+=sh;
+            }
+            if (i==n+ni & j==n+nj) {
                 j-=2; i--;
             }
             else {
-                if (i>=n) {
+                if (i>=n+ni) {
                     i--; j-=2;
                 }
-                if (j>=n) {
+                if (j>=n+nj) {
                     j--;
                 }
             }
-            while (i>=0 & j>=0) {
+            while (i>=0+ni & j>=0+nj) {
                 matrix[i][j]=p;
-                i--; j--; p++;
+                i--; j--; p+=sh;
             }
-            if (i==-1 & j==-1) {
+            if (i==-1+ni & j==-1+nj) {
                 i+=2; j++;
             }
             else {
-                if (i<0) {
+                if (i<0+ni) {
                     i++;
                 }
-                if (j<0) {
+                if (j<0+nj) {
                     j++; i+=2;
                 }
             }
         }
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println("");
-        }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите порядок матрицы");
+        int n = in.nextInt();
+        Double[][] matrix = new Double[2*n][2*n];
+        block(matrix, 0,(double)n/2-1, 0, 0);
+        block(matrix, (double)n/2,n-1, 0, n);
+        block(matrix, 0,(double)n/2-1, n, 0);
+        block(matrix, (double)n/2,n-1, n, n);
+        vivod(matrix);
     }
 }
