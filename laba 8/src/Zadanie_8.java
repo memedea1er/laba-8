56b8d15bd7a1fa5d4554075b5d901a011ad7bb00
Глеб Синьковский <gleb030804@gmail.com>
2023-02-02 12:31:52 +0500

    7

--- laba 8/src/Zadanie_8.java
+++ laba 8/src/Zadanie_8.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
 public class Zadanie_8 {
-    public static void vivod (Double[][] matrix) {
+    public static void vivod (Integer[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                System.out.print(String.format("%.3f", matrix[i][j]).replace(',', '.') + " ");
+                System.out.print(String.format("%3.0f", (double)matrix[i][j]) + " ");
             }
             System.out.println("");
         }
         System.out.println("");
     }
-    public static void block(Double[][] matrix, double na, double ko, int ni, int nj) {
+    public static void block(Integer[][] matrix, int ni, int nj) {
         int n = matrix.length/2, i=0+ni, j=n-1+nj;
-        double p=na, sh=(ko-na)/(n*n-1);
+        int p=1;
         for (int k=0; k<n; k++) {
             while (i<n+ni & j<n+nj) {
                 matrix[i][j]=p;
-                i++; j++; p+=sh;
+                i++; j++; p++;
             }
             if (i==n+ni & j==n+nj) {
                 j-=2; i--;
@@ -31,7 +31,7 @@ public class Zadanie_8 {
             }
             while (i>=0+ni & j>=0+nj) {
                 matrix[i][j]=p;
-                i--; j--; p+=sh;
+                i--; j--; p++;
             }
             if (i==-1+ni & j==-1+nj) {
                 i+=2; j++;
@@ -50,11 +50,11 @@ public class Zadanie_8 {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите порядок матрицы");
         int n = in.nextInt();
-        Double[][] matrix = new Double[2*n][2*n];
-        block(matrix, 0,(double)n/2-1, 0, 0);
-        block(matrix, (double)n/2,n-1, 0, n);
-        block(matrix, 0,(double)n/2-1, n, 0);
-        block(matrix, (double)n/2,n-1, n, n);
+        Integer[][] matrix = new Integer[2*n][2*n];
+        block(matrix, 0, 0);
+        block(matrix, 0, n);
+        block(matrix, n, 0);
+        block(matrix, n, n);
         vivod(matrix);
     }
 }
--- laba 8/src/Zadanie_9.java
+++ laba 8/src/Zadanie_9.java
@@ -4,7 +4,7 @@ public class Zadanie_9 {
     public static void vivod (Integer[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(String.format("%3.0f", (double)matrix[i][j]) + " ");
             }
             System.out.println("");
         }
@@ -17,51 +17,53 @@ public class Zadanie_9 {
             }
         }
     }
-    public static void odin(int n) {
-        Integer[][] matrix = new Integer[n][n];
-        nol(matrix);
+    public static void odin(Integer[][] matrix, int ni, int nj) {
+        int n = matrix.length/2;
         int p=1;
         for (int k=0; k<n/2+1; k++) {
-            int i=k, j=k;
-            while (i<n-k*2) {
+            int i=k+ni, j=k+nj;
+            while (i<n-k*2+ni) {
                 matrix[i][j]=p;
                 i++; p++;
             }
             i--; p--;
-            while (j<n-k & i>k-1) {
+            while (j<n-k+nj & i>k-1+ni) {
                 matrix[i][j]=p;
                 i--; j++; p++;
             }
             i++; j--; p--;
-            while (j>k) {
+            while (j>k+nj) {
                 matrix[i][j]=p;
                 j--; p++;
             }
         }
-        vivod(matrix);
     }
-    public static void dva(int n) {
-        Integer[][] matrix = new Integer[n][n];
-        nol(matrix);
+    public static void dva(Integer[][] matrix, int ni, int nj) {
+        int n = matrix.length/2;
         int p=1;
         for (int k=0; k<n/2+1; k++) {
-            int i=k, j=k;
-            while (i<n-k) {
+            int i=k+ni, j=k+nj;
+            while (i<n-k+ni) {
                 matrix[i][j]=p;
                 i++; p++;
             }
             i--; p--;
-            while (j<n-k) {
+            while (j<n-k+nj) {
                 matrix[i][j]=p;
                 j++; p++;
             }
         }
-        vivod(matrix);
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите порядок матрицы");
         int n = in.nextInt();
-        odin(n); dva(n);
+        Integer[][] matrix = new Integer[2*n][2*n];
+        nol(matrix);
+        odin(matrix, 0, 0);
+        dva(matrix, 0, n);
+        dva(matrix, n, 0);
+        odin(matrix, n, n);
+        vivod(matrix);
     }
 }
